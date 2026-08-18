7d3575c4fe6ef1867065be6866b6052ec2e4c690
lambdaplus <bond010418@gmail.com>
2026-08-18 11:39:40 +0800

    Update after-update-custom.sh
    更改默认ip为192.168.5.155
    更改DHCP下发网段为2-154

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -2,8 +2,9 @@
 # Description: (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.155/g' package/base-files/files/bin/config_generate
 
 # Set DHCP IP Address start and end
 sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
-sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
+sed -i "s/option limit.*/option limit \'153\'/g" package/network/services/dnsmasq/files/dhcp.conf
